6515e8262e6e845b66b2a740988a951b0d352e18
mahmud34033 <abdullahalmahmud033@gmail.com>
2024-04-01 16:21:51 +0600

    first commit

---
+++ 96A - Football.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#include <climits>
+#define ll long long int
+#define nl endl
+#define test() \
+    int t;     \
+    cin >> t;  \
+    while (t--)
+#define forn(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+
+// EOF while(cin >> a >> b >> c){}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    string zero = "0000000";
+    string one = "1111111";
+
+    if((s.find(zero) != string::npos) || (s.find(one) != string::npos)){
+        cout << "YES\n";
+    }
+    else{
+        cout << "NO\n";
+    }
+
+    return 0;
+}
